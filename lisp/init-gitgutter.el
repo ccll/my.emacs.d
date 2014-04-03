63b81dbf4936eb8996a445a85810b661e18bb961
ccll <cailei@live.com>
2014-04-03 16:44:48 +0800

    Replace git-gutter-plus with git-gutter

--- lisp/init-gitgutter.el
+++ lisp/init-gitgutter.el
@@ -1,20 +1,20 @@
-(require 'git-gutter+)
+(require 'git-gutter)
 
 ;; Enable git gutter
-(global-git-gutter+-mode t)
+(global-git-gutter-mode t)
 
 ;; Customize signs
-(setq git-gutter+-modified-sign "!! ")
-(setq git-gutter+-added-sign "++ ")
-(setq git-gutter+-deleted-sign "-- ")
+(setq git-gutter:modified-sign "!! ")
+(setq git-gutter:added-sign "++ ")
+(setq git-gutter:deleted-sign "-- ")
 
 ;; Set color for signs
-(defvar git-gutter-background "lightgrey")
-(set-face-foreground 'git-gutter+-modified "purple")
-(set-face-background 'git-gutter+-modified git-gutter-background)
-(set-face-foreground 'git-gutter+-added "green")
-(set-face-background 'git-gutter+-added git-gutter-background)
-(set-face-foreground 'git-gutter+-deleted "red")
-(set-face-background 'git-gutter+-deleted git-gutter-background)
+(defvar my-git-gutter-background "lightgrey")
+(set-face-foreground 'git-gutter:modified "purple")
+(set-face-background 'git-gutter:modified my-git-gutter-background)
+(set-face-foreground 'git-gutter:added "green")
+(set-face-background 'git-gutter:added my-git-gutter-background)
+(set-face-foreground 'git-gutter:deleted "red")
+(set-face-background 'git-gutter:deleted my-git-gutter-background)
 
 (provide 'init-gitgutter)
