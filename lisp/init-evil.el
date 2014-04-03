3ad87fe4ecd4c8db45dbdae51e17c7bf924c5c93
ccll <cailei@live.com>
2014-04-03 14:15:20 +0800

    Re-enable emacs keys for some minor modes

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -4,8 +4,8 @@
 (evil-mode 1)
 
 ;; Make evil mode key-mapping as initial state for other minor modes
-(setq evil-motion-state-modes (append evil-emacs-state-modes evil-motion-state-modes))
-(setq evil-emacs-state-modes nil)
+;;(setq evil-motion-state-modes (append evil-emacs-state-modes evil-motion-state-modes))
+;;(setq evil-emacs-state-modes nil)
 
 ;; Let ':b' and ':e' open helm window
 (evil-ex-define-cmd "b[uffer]" 'helm-buffers-list)
