955e7693cf6a86743891f4f11ae2a24baf4131fd
ccll <cailei@live.com>
2014-04-03 15:24:29 +0800

    Improve ediff

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -8,4 +8,10 @@
 ;; Show (row:column) in mode line
 (setq column-number-mode t)
 
+;; Split 'ediff' windows side by side
+(setq ediff-split-window-function 'split-window-horizontally)
+
+;; Do not wrap around lines in horizontally split windows
+(setq truncate-partial-width-windows t)
+
 (provide 'init-misc)
