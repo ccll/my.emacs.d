e21a4c4e01132c1f20b60a1741313c500342744c
ccll <cailei@live.com>
2014-04-03 16:26:01 +0800

    Enable git gutter and disable linum-mode, which conflicts to each other

--- lisp/init-gitgutter.el
+++ lisp/init-gitgutter.el
@@ -1,6 +1,19 @@
-(require 'git-gutter-fringe+)
+(require 'git-gutter+)
 
-;; Use minimal skin
-(git-gutter-fr+-minimal)
+;; Enable git gutter
+(global-git-gutter+-mode t)
+
+;; Customize signs
+(setq git-gutter+-modified-sign "!! ")
+(setq git-gutter+-added-sign "++ ")
+(setq git-gutter+-deleted-sign "-- ")
+
+;; Set background color for signs
+(set-face-foreground 'git-gutter+-modified "purple")
+(set-face-background 'git-gutter+-modified "lightgrey")
+(set-face-foreground 'git-gutter+-added "green")
+(set-face-background 'git-gutter+-added "lightgrey")
+(set-face-foreground 'git-gutter+-deleted "red")
+(set-face-background 'git-gutter+-deleted "lightgrey")
 
 (provide 'init-gitgutter)
--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1,6 +1,6 @@
 ;; Enable line number
-(global-linum-mode t)
-(setq linum-format "%4d ")	  ;; Make some room between line number and text
+;;(global-linum-mode t)
+;;(setq linum-format "%4d ")	  ;; Make some room between line number and text
 
 ;; User 'buffer-menu' instead of 'buffer-list'
 (global-set-key (kbd "C-x C-b") 'buffer-menu)
