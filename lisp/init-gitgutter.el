87108b8fbe6b00981d391d74e855aaf44d65d0cb
ccll <cailei@live.com>
2014-04-03 16:29:56 +0800

    Minor improve

--- lisp/init-gitgutter.el
+++ lisp/init-gitgutter.el
@@ -8,12 +8,13 @@
 (setq git-gutter+-added-sign "++ ")
 (setq git-gutter+-deleted-sign "-- ")
 
-;; Set background color for signs
+;; Set color for signs
+(defvar git-gutter-background "lightgrey")
 (set-face-foreground 'git-gutter+-modified "purple")
-(set-face-background 'git-gutter+-modified "lightgrey")
+(set-face-background 'git-gutter+-modified git-gutter-background)
 (set-face-foreground 'git-gutter+-added "green")
-(set-face-background 'git-gutter+-added "lightgrey")
+(set-face-background 'git-gutter+-added git-gutter-background)
 (set-face-foreground 'git-gutter+-deleted "red")
-(set-face-background 'git-gutter+-deleted "lightgrey")
+(set-face-background 'git-gutter+-deleted git-gutter-background)
 
 (provide 'init-gitgutter)
